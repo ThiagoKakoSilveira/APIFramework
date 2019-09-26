499ac8555a3dd505489f6436f12e735d16fa65e6
Thiago <kaksongracie@gmail.com>
2019-09-26 18:33:28 -0300

    Correção com inclusão de caminho absoluto
    Ver depois como alterar a linha 36 por um caminho relativo.

--- src/main/java/br/com/pedidovenda/controller/EnvioPedidoEmailBean.java
+++ src/main/java/br/com/pedidovenda/controller/EnvioPedidoEmailBean.java
@@ -1,20 +1,19 @@
 package br.com.pedidovenda.controller;
 
-import java.io.Serializable;
-import java.util.Locale;
+import br.com.pedidovenda.model.Pedido;
+import br.com.pedidovenda.util.jsf.FacesUtil;
+import br.com.pedidovenda.util.mail.Mailer;
+import com.outjected.email.api.MailMessage;
+import com.outjected.email.impl.templating.velocity.VelocityTemplate;
+import org.apache.velocity.tools.generic.NumberTool;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-
-import org.apache.velocity.tools.generic.NumberTool;
-
-import br.com.pedidovenda.model.Pedido;
-import br.com.pedidovenda.util.jsf.FacesUtil;
-import br.com.pedidovenda.util.mail.Mailer;
-
-import com.outjected.email.api.MailMessage;
-import com.outjected.email.impl.templating.velocity.VelocityTemplate;
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Locale;
 
 @Named
 @RequestScoped
@@ -24,24 +23,24 @@ public class EnvioPedidoEmailBean implements Serializable {
 
 	@Inject
 	private Mailer mailer;
-	
+
 	@Inject
 	@PedidoEdicao
 	private Pedido pedido;
-	
-	public void enviarPedido() {
-		MailMessage message = mailer.novaMensagem();
 
+	public void enviarPedido() throws IOException {
+		MailMessage message = mailer.novaMensagem();
 		message.to(this.pedido.getCliente().getEmail())
-			.subject("Pedido " + this.pedido.getId())
-			.bodyHtml(new VelocityTemplate(String.valueOf(getClass().getResourceAsStream("/emails/pedido.template"))))
-			.put("pedido", this.pedido)
-			.put("numberTool", new NumberTool())
-			.put("locale", new Locale("pt", "BR"))
-			.send();
+				.subject("Pedido " + this.pedido.getId())
+//				.bodyHtml(new VelocityTemplate(getClass().getResourceAsStream("/emails/pedido.template")))
+				.bodyHtml(new VelocityTemplate(new File("D:\\TesteIntelliJ\\PedidoVendaFielAoCurso\\PedidoVenda-master\\src\\main\\resources\\emails\\pedido.template")))
+				.put("pedido", this.pedido)
+				.put("numberTool", new NumberTool())
+				.put("locale", new Locale("pt", "BR"))
+				.send();
 
 		FacesUtil.addInfoMessage("Pedido enviado por e-mail com sucesso!");
 	}
-	
-	
+
+
 }
